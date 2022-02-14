049f52fac5293e345745d807980b15d8854e47a7
laazyCmd <laazy.pr00gramming@protonmail.com>
2022-02-14 10:43:40 +0800

    [NEW] added __error.svelte

---
+++ backend-dashboard/src/routes/__error.svelte
@@ -0,0 +1,36 @@
+<script>
+    import { fade } from 'svelte/transition';
+    import LeftArrow from '$icons/svg/LeftArrow.svelte';
+    import Warning from '$icons/svg/Warning.svelte';
+</script>
+
+<section class="back" 
+in:fade={ { duration: 300 } }>
+    <!-- Back button -->
+    <a class="back-button" 
+    href="/">
+        <div class="back-arrow">
+            <!-- Left arrow SVG component -->
+            <LeftArrow />
+        </div>
+
+        <!-- Back button text -->
+        <div class="back-text">
+            <h3>Go back</h3>
+        </div>
+    </a>
+</section>
+
+<section class="loading">
+    <header class="loading-header">
+        <!-- Loading animated SVG component -->
+        <figure class="flex justify-center">
+            <Warning class={ "text-center scale-[7.5] m-auto fill-yellow-400" }/>
+        </figure>
+
+        <!-- Loading messages -->
+        <div class="loading-error">
+            <h3 class="loading-error-message">This page does not exist; go back to the dashboard</h3>
+        </div>
+    </header>
+</section>
--- backend-dashboard/src/routes/table/__error.svelte
+++
@@ -1,1 +0,0 @@
-<div>sadsasad</div>
