b8368a95f257127b20a65e02398b32fc14c88ce0
laazyCmd <laazy.pr00gramming@protonmail.com>
2022-02-14 15:24:09 +0800

    [STYLE] migrated inline css styles to app.css

--- backend-dashboard/src/components/table/TablePage.svelte
+++ backend-dashboard/src/components/table/TablePage.svelte
@@ -4,9 +4,10 @@
 </script>
 
 <!-- Pages list -->
-<section class="flex justify-center mt-6 mb-5">
-    <div class="flex items-center space-x-2.5">
-        <button class="top-1/2 font-semibold text-lg text-black bg-blue-200 rounded-md px-3 cursor-pointer py-1.5 hover:bg-blue-300 duration-300"
+<section class="page-section">
+    <div class="pages">
+        <button class="page-button"
+        class:page-button-disabled={ page === 0 }
         on:click={ switchPageRow( page, true ) }
         disabled={ page === 0 }>
             <span>←</span>
@@ -16,7 +17,8 @@
             { page + 1 }
         </div>
         
-        <button class="top-1/2 font-semibold text-lg text-black bg-blue-200 rounded-md px-3 cursor-pointer py-1.5 hover:bg-blue-300 duration-300"
+        <button class="page-button"
+        class:page-button-disabled={ lastPage }
         on:click={ switchPageRow( page, false ) }
         disabled={ lastPage }>
             <span>→</span>
--- backend-dashboard/src/components/table/TableSearch.svelte
+++ backend-dashboard/src/components/table/TableSearch.svelte
@@ -5,9 +5,9 @@
     let specifiedColumn, searchValue;
 </script>
 
-<section class="flex justify-start my-5 space-x-3">
+<section class="search-section">
     <!-- Search input -->
-    <input class="rounded-md pl-2.5 pr-3.5 w-full outline-none" type="text" placeholder="Type a query here" bind:value={ searchValue }>
+    <input class="search-input" type="text" placeholder="Type a query here" bind:value={ searchValue }>
 
     <!-- Column box -->
     <select name="columns" id="columns" bind:value={ specifiedColumn }>
@@ -17,7 +17,7 @@
     </select>
 
     <!-- Search button -->
-    <button class="top-1/2 font-semibold text-lg text-gray-700 bg-gray-200 rounded-md px-6 py-2.5 hover:bg-gray-300 duration-300"
+    <button class="search-button"
     on:click={ searchRow( specifiedColumn, searchValue ) }>
         <Search />
     </button>
