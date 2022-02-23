09f2bd5068d4b48fd39321565605c74707665a27
laazyCmd <laazy.pr00gramming@protonmail.com>
2022-02-23 22:19:00 +0800

    [DEP] updated svelte-kit version -> 208; added static adapter

--- backend-dashboard/svelte.config.js
+++ backend-dashboard/svelte.config.js
@@ -1,3 +1,4 @@
+import adapter from '@sveltejs/adapter-static';
 import preprocess from "svelte-preprocess";
 import { resolve } from 'path';
 
@@ -13,6 +14,14 @@ const config = {
         }
       }
     },
+    
+    // Use static adapter
+    adapter: adapter( {
+      pages: 'build',
+      assets: 'build',
+      fallback: null,
+      precompress: true
+    } ),
 
     // hydrate the <div id="svelte"> element in src/app.html
     target: "#svelte",
